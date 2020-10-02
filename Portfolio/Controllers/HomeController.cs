65390c9a577505cd986551dcc9543ff254aad2a1
Lesiezhou2020 <lesliezhou2012@gmail.com>
2020-10-01 20:12:17 -0700

    submit post needed

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet("/projects")]
+        public ViewResult Projects()
+        {
+            return View("Projects");
+        }
+
+        [HttpGet("/contact")]
+
+        public ViewResult Contact()
+        {
+            return View("Contact");
+        }
+
+        
+    }
+
+
+}
